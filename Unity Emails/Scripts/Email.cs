118ecdff5610fcfb45befca66c98c03b75a9947e
nftpixels <97366705+nftpixels@users.noreply.github.com>
2022-12-14 15:02:54 +1300

    Add files via upload

---
+++ Unity Emails/Scripts/Email.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace UE.Email
+{
+    /// <summary>
+    /// This class allows sending emails in the browser using an external javasript library.
+    /// Based on https://smtpjs.com/ API
+    /// </summary>
+    public static partial class Email
+    {
+        private const string BuildWarning = "Sending Emails is only supported in WebGL build.";
+
+        [DllImport("__Internal")]
+        private static extern void SendMail(string from, string to, string subject, string body, string smtp,
+            string user,
+            string password);
+
+        [DllImport("__Internal")]
+        private static extern void SendMailToken(string from, string to, string subject, string body, string token);
+
+        [DllImport("__Internal")]
+        private static extern void SendMailTokenWithAttachment(string from, string to, string subject, string body,
+            string token, string attachment);
+
+        /// <summary>
+        /// Sends an email with the given credentials.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        /// <param name="smtp">Address of the smtp server</param>
+        /// <param name="user">Username on the smtp server</param>
+        /// <param name="password"></param>
+        public static void SendEmail(string from, string to, string subject, string body, string smtp, string user,
+            string password)
+        {
+            SendMail(from, to, subject, body, smtp, user, password);
+        }
+
+        /// <summary>
+        /// Sends an email with the given token.
+        /// The token can be generated at https://smtpjs.com/
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        /// <param name="token"></param>
+        public static void SendEmailToken(string from, string to, string subject, string body, string token)
+        {
+            SendMailToken(from, to, subject, body, token);
+        }
+
+        /// <summary>
+        /// Sends an email with the given token and an attachment as byte array.
+        /// The token can be generated at https://smtpjs.com/
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        /// <param name="token"></param>
+        /// <param name="attachment"></param>
+        public static void SendEmailToken(string from, string to, string subject, string body, string token,
+            byte[] attachment)
+        {
+            Debug.LogWarning(BuildWarning);
+        }
+
+        /// <summary>
+        /// Encodes the given byte array to the dataURI scheme (see https://en.wikipedia.org/wiki/Data_URI_scheme).
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private static string EncodeDataUri(byte[] data)
+        {
+            return "data:text/plain;charset=US-ASCII;base64," + Convert.ToBase64String(data);
+        }
+    }
+}
---
+++ Unity Emails/SendEmail.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UE.Email;
+using System;
+
+public class SendEmail : MonoBehaviour
+{
+
+    // Example script for sending emails based on any user specified event
+
+    // Public variables for the editor
+
+    [Header("Email Server Configuration")]
+
+    [Tooltip("The outgoing server.")]
+    public string smtp;
+
+    [Tooltip("This is the username for the email address you want to send from. Don't share this information with anyone! ")]
+    public string username;
+
+    [Tooltip("This is the password for the email address you want to send from. Don't share this information with anyone! ")]
+    public string password;
+
+
+    [Header("Email Message Configuration")]
+
+    [Tooltip("Who is sending this message?")]
+    public string from;
+
+    [Tooltip("To who are you sending this message?")]
+    public string to;
+
+    [Tooltip("What is the message subject? ")]
+    public string subject;
+
+    [Tooltip("You can use HTML.Bold() or HTML.Italic() in the body/message.")]
+    public string message;
+
+    public string token = "e9e5fee4-29c3-4c4d-9a22-561b3c2689f7";
+
+    private void Start()
+    {
+        smtp = "SMTPServerHere";
+        username = "EmailUsernameHere";
+        password = "EmailPasswordHere";
+
+        from = "FromEmail";
+        to = "ToEmail";
+        subject = "Security Notification";
+        message = "This is a notification to let you know that we've detected suspicious activity";
+
+        Debug.Log(from);
+        Debug.Log(to);
+        Debug.Log(subject);
+        Debug.Log(message);
+        Debug.Log(smtp);
+        Debug.Log(username);
+        Debug.Log(password);
+    }
+
+    // When the user clicks the button, an email gets sent
+    public void SendWebmail ()
+    {
+        try
+        {
+            Email.SendEmailToken(from, to, subject, message, token);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+}
